facd4f626ef3fd421b603bea740e4d7a1a8c300a
BorAlex <alexander44@bk.ru>
2022-12-25 12:44:03 +0300

    seminar3_2_2 new version

---
+++ seminar3_2_2.py
@@ -0,0 +1,27 @@
+from random import sample
+
+
+def list_rand_num(count: int) -> list:
+    if count < 0:
+        print("Negative value of the number of numbers")
+        return []
+
+    list_nums = sample(range(1, count*2), count)
+    return list_nums
+
+
+def prod_pairs(list_nums: list):
+    res_list = []
+    len_list = len(list_nums)
+
+    for k in range(len_list // 2):
+        res_list.append(list_nums[k] * list_nums[len_list - k -1])
+
+    if len_list % 2:
+        res_list.append(list_nums[len_list // 2])
+    return res_list
+
+
+all_list = list_rand_num(int(input("Number of numbers: ")))
+print(all_list)
+print(prod_pairs(all_list))
